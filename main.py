8e94f7d7d823856c9698c3c169a9eae3043bcfea
Alexfox36 <klyuev_aleksandr@mail.ru>
2025-11-05 16:39:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from pprint import pprint
+
+main_page = "https://habr.com/ru/articles/top/daily/"
+
+def get_url_html(url: str) -> str:
+    res = requests.get(
+        url,
+        headers={
+            "User-Agent": UserAgent().google,
+    })
+    return res.text
+
+def get_soup(html_text: str) -> BeautifulSoup:
+    return BeautifulSoup(html_text, "lxml")
+
+@dataclass
+class ArticleData:
+    title: str
+    views: str
+    url: str
+    text: str
+
+
+
+def get_all_posts(soup:BeautifulSoup) -> list[ArticleData]:
+    posts_data = []
+    all_articles_soup = soup.find_all("article", class_="tm-articles-list__item")
+    for article_soup in all_articles_soup:
+        articles_title = article_soup.find("a", class_="tm-title__link").find("span").text
+        articles_views = article_soup.find("span", class_="tm-icon-counter__value").text
+        article_url: str = article_soup.find('a', class_='tm-title__link')['href']
+        article_url = 'https://habr.com' + article_url
+        article_text: str = get_article_text(article_url)
+        posts_data.append(ArticleData(
+            title=articles_title,
+            views=articles_views,
+            url=article_url,
+            text=article_text,
+        ))
+    return posts_data
+
+def get_article_text(url: str) -> str:
+    try:
+        article_html = get_url_html(url)
+        soup = get_soup(article_html)
+        article_text = soup.find('article', class_= 'tm-article-presenter__content')
+        if article_text:
+            return article_text.get_text(separator='\n', strip=True)
+        return "Текст статьи не найден"
+    except Exception as e:
+        return f"Ошибка при получении текста: {str(e)}"
+
+
+
+def main():
+    html = get_url_html(main_page)
+    soup = get_soup(html)
+    posts = get_all_posts(soup)
+    pprint(posts)
+
+
+if __name__ == "__main__":
+    main()
